80b8296422477f1536762ee904ba6dfb85a66788
Callum Guthrie <cgwguthrie93@gmail.com>
2016-02-28 13:52:21 +0000

    Comitting so can work on report

--- Mongo/mongo.py
+++ Mongo/mongo.py
@@ -7,29 +7,31 @@ connection = MongoClient()
 db = connection["mongomodel3"]
 emage = db["emage"]
 
-with open("emage_submissions.tsv") as file1:
-    reader1 = csv.DictReader(file1, delimiter="\t")
+with open("Data/Assays.csv") as file1:
+    reader1 = csv.DictReader(file1, delimiter=",")
     for row in reader1:
       emage.insert({
-         '_id': int(row['EMAGE_ID']),
-         'probe_id':row['Probe_ID'],
-         'assay_type':row['Assay_Type'],
-         'assay_source':row['Assay_Source'],
-         'specimen' : {'specimen_type':row['Specimen_Type'],'specimen_strain':row['Specimen_Strain']},
-         'stage' : {'theiler_stage':row['Theiler_Stage'],'user_stage':row['User_Stage']}})
+         '_id': int(row['emage_id']),
+         'probeID':row['probe_id'],
+         'type':row['assay_type'],
+         'source':row['name'],
+         'specimen' : {'type':row['type'],'strain':row['strain']},
+         'stage' : {'theilerstage':int(row['theilerstage']),'dpc':row['dpc']}})
 
-with open("emage_publications.tsv") as file2:
-   reader2 = csv.DictReader(file2, delimiter="\t")
+
+with open("Data/Publications.csv") as file2:
+   reader2 = csv.DictReader(file2, delimiter=",")
    for row in reader2:
-      emage.update_many({'_id': int(row['EMAGE_ID'])},
-         {'$push' : {'publication' : {'publication_id' : row['Publication_ID'], 'publication_title' : row['Title'], 'publication_author' : row['Authors']}}},
+      emage.update_many({'_id': int(row['emage_id'])},
+         {'$push' : {'publication' : {'publicationID' : int(row['accession']), 'title' : row['title'], 'author' : row['author']}}},
          upsert=False)
 
-with open("emage_annotations.tsv") as file3:
-   reader3 = csv.DictReader(file3, delimiter="\t")
+
+with open("Data/TextAnnotations.csv") as file3:
+   reader3 = csv.DictReader(file3, delimiter=",")
    for row in reader3:
-      emage.update_many({'_id': int(row['EMAGE_ID'])},
-         {'$push' : { 'textannotation' : {'strength' : row['Strength'],
-         'anatomystructure' : {'term_id' : int(row['Term_ID']),'term' : row['Term']},
-         'gene' : {'gene_id' : row['Gene_ID'],'symbol' : row['Symbol']}}}},
+      emage.update_many({'_id': int(row['emage_id'])},
+         {'$push' : { 'textannotation' : {'strength' : row['strength'],
+         'anatomystructure' : {'structureID' : int(row['EMAPA']),'term' : row['term']},
+         'gene' : {'geneID' : row['accession'],'name' : row['name']}}}},
          upsert=False)
